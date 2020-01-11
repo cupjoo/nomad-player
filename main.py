ed3059bcf6e69611fc94be0e051d6ffca891621e
cupjoo <cupjoo@naver.com>
2020-01-11 20:51:53 +0900

    add error handler

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
+import platform
+import sys
 from bs4 import BeautifulSoup
-from urllib.parse import urlencode
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 class Migrator:
     def __init__(self):
         chrome_options = webdriver.ChromeOptions()
         chrome_options.headless = True
-        chrome_options.add_argument('--headless') 
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_argument('window-size=1920x1080')
-        self.driver = webdriver.Chrome('../chromedriver', options=chrome_options)
+        driver_path = '../chromedriver'
+        if "Windows" in platform.platform():
+            chrome_options.add_argument('--headless')
+            driver_path += '.exe'
+        self.driver = webdriver.Chrome(driver_path, options=chrome_options)
         self.driver.implicitly_wait(3)
-        
+
     def scrap_list(self, seq):
         try:
-            melon_link='https://www.melon.com/mymusic/dj/mymusicdjplaylistview_inform.htm'
-            melon_link+='?plylstSeq='+seq
+            melon_link = 'https://www.melon.com/mymusic/dj/mymusicdjplaylistview_inform.htm'
+            melon_link += '?plylstSeq=' + seq
             self.driver.get(melon_link)
             self.driver.get_screenshot_as_file('melon.png')
+            print('scrap successed')
         except:
-            print('failed to load playlist')
-            self.shutdown()
-    
+            self.shutdown('failed to load playlist')
+
     def login(self, email, password):
         try:
             # 벅스 아이디 로그인
@@ -36,10 +41,12 @@ class Migrator:
             self.driver.find_element_by_id('passwd').send_keys(password)
             self.driver.find_element_by_xpath('//*[@id="frmLoginLayer"]/div/div/button').click()
             self.driver.find_element_by_xpath('//*[@id="frmLoginLayer"]/div/div/button').click()
+            WebDriverWait(self.driver, 5)\
+                .until(EC.presence_of_element_located((By.XPATH, '//*[@id="loginHeader"]/a[1]')))
+            print('login successed')
         except:
-            print('failed to login in')
-            self.shutdown()
-    
+            self.shutdown('failed to login in')
+
     def add_playlist(self):
         plist = ['painkiller']
         for song in plist:
@@ -47,19 +54,22 @@ class Migrator:
                 self.driver.find_element_by_id('headerSearchInput').clear()
                 self.driver.find_element_by_id('headerSearchInput').send_keys(song)
                 self.driver.find_element_by_id('hederSearchFormButton').click()
-                soup = bs4(self.driver.page_source, 'html.parser')
-                print(self.driver.find_element_by_xpath('//*[@id="DEFAULT0"]/table/tbody/tr[1]/td[8]/a')
-                      .get_attribute("onclick"))
-                # self.driver.get_screenshot_as_file('bugs.png')
+                self.driver.find_element_by_xpath('//*[@id="DEFAULT0"]/table/tbody/tr[1]/td[8]/a').click()
+                WebDriverWait(self.driver, 2)\
+                    .until(EC.presence_of_element_located((By.XPATH, '//*[@id="track2playlist"]')))
+                self.driver.get_screenshot_as_file('bugs.png')
             except:
-                print('failed to add playlist')
-                self.shutdown()
-    
-    def shutdown(self):
+                self.shutdown('failed to add playlist')
+
+    def shutdown(self, msg):
+        print(msg)
+        self.driver.get_screenshot_as_file('fails.png')
         self.driver.close()
+        sys.exit()
+
 
 migrator = Migrator()
-#migrator.scrap_list('466034240')
-migrator.login('', '')
+# migrator.scrap_list('466034240')
+migrator.login('cupjoo@naver.com', 'chl010177@')
 migrator.add_playlist()
-migrator.shutdown()
+migrator.shutdown('migration successed!')
